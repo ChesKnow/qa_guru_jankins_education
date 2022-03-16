82113b7b023e80d8387e610d9ef5da78c860e665
ChesKnow <Roman.Chesanov@outlook.com>
2022-03-16 23:35:49 +0300

    added Attachments

--- src/test/java/tests/demoqa/TestBase.java
+++ src/test/java/tests/demoqa/TestBase.java
@@ -2,7 +2,9 @@ package tests.demoqa;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
@@ -21,5 +23,13 @@ public class TestBase {
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @AfterEach
+    void addAttachment() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();;
+        Attach.addVideo();
     }
 }
