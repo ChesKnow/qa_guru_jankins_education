89ff7697b0a7c1a9c33e8cba482911c5c5d1e45c
ChesKnow <Roman.Chesanov@outlook.com>
2022-03-18 22:11:30 +0300

    added getProperty

--- src/test/java/tests/homework1/RegistrationForm.java
+++ src/test/java/tests/homework1/RegistrationForm.java
@@ -39,7 +39,7 @@ public class RegistrationForm {
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = System.getProperty("browser_address");
     }
 
     @AfterEach
